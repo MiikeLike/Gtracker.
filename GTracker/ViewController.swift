12c9783872837c0960deb679e0772f31bc8adaee
MiikeLike <mak_valle@hotmail.es>
2023-10-24 18:25:12 +0200

    tableView Union
    Union de las tableView creando una unificada

--- GTracker/ViewController.swift
+++ GTracker/ViewController.swift
@@ -5,101 +5,89 @@
 //  Created by Mikel Valle on 20/10/23.
 //
 import UIKit
+import Charts
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
-    @IBOutlet weak var gastosTableView: UITableView!
-    @IBOutlet weak var ingresosTableView: UITableView!
-    @IBOutlet weak var nombreGastoTextField: UITextField!
-    @IBOutlet weak var montoGastoTextField: UITextField!
-    @IBOutlet weak var nombreIngresoTextField: UITextField!
-    @IBOutlet weak var montoIngresoTextField: UITextField!
-    @IBOutlet weak var viewBlue: UIImageView!
-    @IBOutlet weak var textField: UITextField!
-
-    var gastos: [Gasto] = []
-    var ingresos: [Ingreso] = []
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var nombreTextField: UITextField!
+    @IBOutlet weak var montoTextField: UITextField!
+    @IBOutlet weak var viewBlue: UIImageView!    
+    var registrosFinancieros: [registroFinanciero] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Gastos y Ingresos"
-        gastosTableView.delegate = self
-        gastosTableView.dataSource = self
-        ingresosTableView.delegate = self
-        ingresosTableView.dataSource = self
-
-        gastosTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        ingresosTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
 
         // Asigna el controlador de delegado del UITextField
-        textField?.delegate = self
-        
+        nombreTextField.delegate = self
+        montoTextField.delegate = self
+
         // Agrega un gesto de toque a la vista principal para cerrar el teclado
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tapGesture)
-        
-        
-        gastosTableView.layer.cornerRadius = 10.0
-        ingresosTableView.layer.cornerRadius = 10.0
-        viewBlue.layer.cornerRadius = 09.0
+
+        tableView.layer.cornerRadius = 10.0
+        viewBlue.layer.cornerRadius = 10.0
+    }
+    //Creación de función para que el textField de Dinero solo entren datos tipo String
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == montoTextField || textField == montoTextField {
+            let allowedCharacters = CharacterSet.decimalDigits // Solo números
+            let characterSet = CharacterSet(charactersIn: string)
+            return allowedCharacters.isSuperset(of: characterSet)
+        }
+        return true
     }
 
-    @IBAction func agregarGasto(_ sender: UIButton) {
-        if let nombre = nombreGastoTextField.text, let monto = Double(montoGastoTextField.text ?? "") {
-            gastos.append(Gasto(nombre: nombre, monto: NSDecimalNumber(value: monto)))
-            gastosTableView.reloadData()
-            nombreGastoTextField.text = ""
-            montoGastoTextField.text = ""
+
+    @IBAction func agregarRegistroFinanciero(_ sender: UIButton) {
+        if let nombre = nombreTextField.text, let monto = Double(montoTextField.text ?? "") {
+            let tipo: tipoRegistro = monto >= 0 ? .ingreso : .gasto
+            registrosFinancieros.append(registroFinanciero(nombre: nombre, monto: NSDecimalNumber(value: monto), tipo: tipo))
+            tableView.reloadData()
+            nombreTextField.text = ""
+            montoTextField.text = ""
         }
     }
 
-    @IBAction func agregarIngreso(_ sender: UIButton) {
-        if let nombre = nombreIngresoTextField.text, let monto = Double(montoIngresoTextField.text ?? "") {
-            ingresos.append(Ingreso(nombre: nombre, monto: NSDecimalNumber(value: monto)))
-            ingresosTableView.reloadData()
-            nombreIngresoTextField.text = ""
-            montoIngresoTextField.text = ""
-        }
-    }
-    //Lógica tableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if tableView == gastosTableView {
-            return gastos.count
-        } else {
-            return ingresos.count
-        }
+        return registrosFinancieros.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        if tableView == gastosTableView {
-            let gasto = gastos[indexPath.row]
-            cell.textLabel?.text = "\(gasto.nombre): \(gasto.monto)€"
-        } else {
-            let ingreso = ingresos[indexPath.row]
-            cell.textLabel?.text = "\(ingreso.nombre): \(ingreso.monto)€"
-        }
+        let registro = registrosFinancieros[indexPath.row]
+
+        cell.textLabel?.text = "\(registro.nombre): \(registro.tipo == .ingreso ? "+" : "-")\(registro.monto)€"
+        cell.textLabel?.textColor = registro.tipo == .ingreso ? .green : .red
+
         return cell
     }
-    //Fin lógica tableView
-    
+
     // Función para cerrar el teclado
-      @objc func dismissKeyboard() {
-          view.endEditing(true)
-      }
-      
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
     // Implementa el delegado del UITextField para permitir que se cierre el teclado
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-          textField.resignFirstResponder()
-          return true
-      }
+        textField.resignFirstResponder()
+        return true
+    }
+
+
 }
 
-struct Gasto {
+struct registroFinanciero {
     var nombre: String
     var monto: NSDecimalNumber
+    var tipo: tipoRegistro
 }
 
-struct Ingreso {
-    var nombre: String
-    var monto: NSDecimalNumber
+enum tipoRegistro {
+    case ingreso
+    case gasto
 }
